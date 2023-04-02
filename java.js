478d114ec44447ea71cbb169f79939849677f322
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2023-04-02 22:26:39 +1000

    rewrite better the code

--- java.js
+++ java.js
@@ -2,8 +2,13 @@ const libraryMain = document.querySelector(".library-main");
 const popup = document.querySelector(".add_popup");
 const bookInfo = document.getElementById("total_books");
 const PagesInfo = document.getElementById("total_pages");
-const CompleteBookInfo = document.getElementById("completed_books");
+const TotalCompletePagesInfo = document.getElementById("total_completed_pages");
 const OngoingBooksInfo = document.getElementById("ongoing_books");
+const titleForm = document.getElementById("title");
+const authorForm = document.getElementById("author");
+const pagesForm = document.getElementById("pages");
+const completed_pagesForm = document.getElementById("completed_pages");
+
 let myLibrary = [];
 
 const addBook = document
@@ -25,69 +30,91 @@ const submitButton = document
     createbook();
     updateInfo();
     popup.style.visibility = "hidden";
-    createEventListener()
 })
 
 function createbook(){
-    form = Array.from(document.querySelectorAll("#book_data input")).reduce((acc, input) =>({...acc, [input.id]: input.value}), {});
-    // book = Object.assign({}, form)
-    // myLibrary.push(book)
-    const newbook = new book (form.title, form.author, form.total_pages, form.completed_pages);
+    let Title = titleForm.value;
+    let author = authorForm.value
+    let pages = pagesForm.value
+    let completed_pages = completed_pagesForm.value
+    let id = myLibrary.length
+    
+    const newbook = new book (Title, author, pages, completed_pages, id);
     myLibrary.push(newbook);
     newbook.appendBook(newbook);
 }
 
-function book(title, author, total_pages, completed_pages){ // dont think i need this but just in case
+function book(title, author, total_pages, completed_pages, id){ 
     this.title = title;
     this.author = author;
     this.total_pages = total_pages;
     this.completed_pages = completed_pages;
+    this.id = id;
 }
 
 
 book.prototype.appendBook = (newbook) =>{
-    let booknode = document.createElement("div")
-    booknode.classList.add("book");
-    booknode.innerHTML = (`
-    <div>
-        <h3 class="title">${newbook.title}</h3>
-    </div>
-    <div>
-        <h4>Author:</h4>
-        <p>${newbook.author}</p>
-        <h4>Total Pages:</h4>
-        <p>${newbook.total_pages}</p>
-        <h4>Completed Pages:</h4>
-        <p>${newbook.completed_pages}</p>
-    </div>
-    <div>
-    <button class="edit"">Edit</button>
-    <button class="delete">Delete</button>
-    </div>`)
+    let nodeBook = document.createElement("div");
+    let nodeHeader = document.createElement("div");
+    let nodeTitle = document.createElement("h3");
+    let nodeDiv = document.createElement("div");
+    let nodeAuthor = document.createElement("h4");
+    let nodeAuthorContent = document.createElement("p");
+    let nodeTotalPages = document.createElement("h4");
+    let nodeTotalPagesContent = document.createElement("p");
+    let nodeCompletedPages = document.createElement("h4");
+    let nodeCompletedPagesContent = document.createElement("p");
+    let nodeButtonDiv = document.createElement("div");
+    let nodeEditButton = document.createElement("button");
+    let nodeDeleteButton = document.createElement("button");
 
-    libraryMain.append(booknode);
+    nodeBook.classList.add("book");
+    nodeEditButton.classList.add("edit");
+    nodeDeleteButton.classList.add("delete");
+
+    nodeTitle.textContent = newbook.title;
+    nodeAuthor.textContent = "Author:";
+    nodeAuthorContent.textContent = newbook.author;
+    nodeTotalPages.textContent = "Total Pages:";
+    nodeTotalPagesContent.textContent = newbook.total_pages;
+    nodeCompletedPages.textContent = "Completed Pages:";
+    nodeCompletedPagesContent.textContent = newbook.completed_pages;
+    nodeEditButton.textContent = "Edit";
+    nodeDeleteButton.textContent = "Delete";
+
+
+    libraryMain.append(nodeBook);
+    nodeBook.append(nodeHeader)
+    nodeHeader.append(nodeTitle);
+    nodeBook.append(nodeDiv);
+    nodeDiv.append(nodeAuthor);
+    nodeDiv.append(nodeAuthorContent);
+    nodeDiv.append(nodeTotalPages);
+    nodeDiv.append(nodeTotalPagesContent);
+    nodeDiv.append(nodeCompletedPages);
+    nodeDiv.append(nodeCompletedPagesContent);
+    nodeBook.append(nodeButtonDiv);
+    nodeButtonDiv.append(nodeEditButton);
+    nodeButtonDiv.append(nodeDeleteButton);
+
+    nodeEditButton.addEventListener("click", function(){
+        editPage(newbook)
+    })
+
+
 }
 
 function updateInfo (){
     bookInfo.innerHTML = 0
     PagesInfo.innerHTML = 0
-    CompleteBookInfo.innerHTML = 0
+    TotalCompletePagesInfo.innerHTML = 0
     for(i = 0; i < myLibrary.length; i++){
         bookInfo.innerHTML++
         PagesInfo.innerHTML = +PagesInfo.innerHTML + +myLibrary[i].total_pages;
-        CompleteBookInfo.innerHTML = +CompleteBookInfo.innerHTML + +myLibrary[i].completed_pages;
+        TotalCompletePagesInfo.innerHTML = +TotalCompletePagesInfo.innerHTML + +myLibrary[i].completed_pages;
     }
 }
 
-function createEventListener() {
-   title = document.getElementsByClassName("title").innerText
-   console.log(title)
-    Array.from(document.getElementsByClassName("edit")).forEach(function(button) {
-      button.addEventListener("click", function() {
-        let editObject = myLibrary.find((obj) => obj.title === title.innerHTML);
-        console.log(editObject)
-      });
-    });
-  }
-
-  
+function editPage(bookData){
+    console.log(bookData.id);
+}
